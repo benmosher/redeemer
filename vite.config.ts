4d7aa2cdf6ae644b028f0f1b5f8021171e096019
Ben Mosher <me@benmosher.com>
2025-09-07 12:08:07 -0400

    gracefully degrade when no ssl cert generated
    [skip ci]

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,23 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import fs from "fs";
 
+function trySSL() {
+  try {
+    return {
+      https: {
+        key: fs.readFileSync(".cert/key.pem"),
+        cert: fs.readFileSync(".cert/cert.pem"),
+      },
+      host: true, // allows LAN access
+    };
+  } catch (err) {
+    console.warn("no SSL cert found, falling back to HTTP");
+    return undefined;
+  }
+}
+
 export default defineConfig({
   plugins: [react()],
   build: { outDir: "build" },
-  server: {
-    https: {
-      key: fs.readFileSync(".cert/key.pem"),
-      cert: fs.readFileSync(".cert/cert.pem"),
-    },
-    host: true, // allows LAN access
-  },
+  server: trySSL(),
 });
